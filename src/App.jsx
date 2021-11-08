f6f9bc1c1f5250437488d1677fa75aa53c4aa94f
E-Kuru <evandalleaublop@gmail.com>
2021-11-08 14:50:20 +0100

    First Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
+
 import Player from './components/Player/Player';
 import Grid from './components/Grid/Grid';
+import Header from './components/header/Header';
 
 import './App.css'
 export default class App extends Component {
   render() {
     return (
       <>
-        {/* <h1>hello</h1> */}
+          <Header title='Puissance 4'/>
         <div id="players">
         <Player number='1'/>
         <Player number='2'/>
