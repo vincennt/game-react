32177117877cfec8971a33c54179e34301e8e8b3
E-Kuru <evandalleaublop@gmail.com>
2021-11-08 14:11:02 +0100

    Player css created

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <h1>hello</h1>
+        {/* <h1>hello</h1> */}
         <Player/>
         <Grid/>
       </>
