519f5374bfda01d64acce56b6e9a142ebbe6fad8
E-Kuru <evandalleaublop@gmail.com>
2021-11-08 14:43:58 +0100

    players done

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
+import './player.css'
+
 export default class Player extends Component {
+
     render() {
+        const {number, img} = this.props
         return (
-            <div>
-                <h2>Player Component</h2>
+            <div className='player'>
+                <div style={{background :  `url("https://i.pravatar.cc/300?img=${Math.random()}") no-repeat center/cover`,
+                 width : '45%', height : '100%'}}></div>
+                <h2>Player {number}</h2>
             </div>
         )
     }
