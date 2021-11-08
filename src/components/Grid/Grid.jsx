15ec21f1d022cdaac45d05993119a478e73b51d1
Vincent <vins@Pimentas-MacBook-Air.local>
2021-11-08 12:21:44 +0100

    files and repo created

---
+++ src/components/Grid/Grid.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Grid extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Grid</h3>
+            </div>
+        )
+    }
+}
---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Player extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Player Component</h2>
+            </div>
+        )
+    }
+}
+
+
+
