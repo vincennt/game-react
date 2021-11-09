ceeed903fd101d57273db1c6003f57eabc37e672
Vincent <vins@Pimentas-MacBook-Air.local>
2021-11-09 11:04:51 +0100

    grid style

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -5,9 +5,9 @@ export default class Grid extends Component {
     render() {
         const {basic} = this.props
         return (
-            <div className="container">
-                <div className="row justify-content-center">{basic.map( (e,i) => (
-                        <div className="colone col-1" key={i} id={i}>{e.map( (e,i) => (
+            <div className="container d-flex justify-content-center mt-4">
+                <div className="row justify-content-center grid-height">{basic.map( (e,i) => (
+                        <div className="colone " key={i} id={i}>{e.map( (e,i) => (
                             <div className="circle ligne" key={i} id={i}></div>))}
                         </div>
                     ))}
