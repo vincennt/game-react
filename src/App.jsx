2061dadfa252e201f4d2decf006de05691450272
HanaaChouiter <hanaa7chouiter@gmail.com>
2021-11-10 16:21:00 +0100

    'jdb'

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Header from './components/header/Header'
 import Player from './components/Player/Player'
