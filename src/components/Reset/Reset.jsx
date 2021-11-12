c67d6ebefedab1f368cc29b699d872a4584023a0
E-Kuru <evandalleaublop@gmail.com>
2021-11-12 12:09:28 +0100

    Conflicts adjust

--- src/components/Reset/Reset.jsx
+++ src/components/Reset/Reset.jsx
@@ -13,6 +13,8 @@ class Reset extends Component {
             <audio loop autoPlay
                 src='./time.mp3'>
             </audio>
+            <audio id="audio" src="../../mario-coin-sound-effect.mp3"></audio>
+
 
             <button className={joueur ? 'button border border-5 border-warning' : 'button border border-5  border-danger'}
             onClick={handleClick}
