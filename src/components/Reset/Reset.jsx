3fc4ec1a6b078b7fa09bd4c313d163e55f3dca69
E-Kuru <evandalleaublop@gmail.com>
2021-11-12 11:11:30 +0100

    Musique added

--- src/components/Reset/Reset.jsx
+++ src/components/Reset/Reset.jsx
@@ -9,6 +9,11 @@ class Reset extends Component {
             <button className={joueur ? 'button border border-5 border-warning' : 'button border border-5  border-danger'}
             onClick={homeClick}
             >Acceuil </button>
+            
+            <audio controls loop autoPlay
+                src='./time.mp3'>
+            </audio>
+
             <button className={joueur ? 'button border border-5 border-warning' : 'button border border-5  border-danger'}
             onClick={handleClick}
             >Restart</button>
